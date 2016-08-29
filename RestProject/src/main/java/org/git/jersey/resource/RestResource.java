85135a10940cdf2375bc000bf62d98725a432761
sujittripathy <sujittripathy@192.168.0.3>
2016-08-28 23:42:36 -0700

    8/28 PM delivery

--- RestProject/src/main/java/org/git/jersey/resource/RestResource.java
+++ RestProject/src/main/java/org/git/jersey/resource/RestResource.java
@@ -1,15 +1,17 @@
 package org.git.jersey.resource;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.git.jersey.model.Policy;
 
-@Path("/calculator")
+@Path("calculator")
 public class RestResource {
 
 		@GET
@@ -17,13 +19,29 @@ public class RestResource {
 			return "Test String";
 		}
 		
+		@GET
+		@Path("{name}")
+		@Produces({MediaType.APPLICATION_JSON})
+		public String testPost(@PathParam("name") String val){
+			return "hello" + val;
+		}
+		
 		@POST
-		@Path("/add")
-		@Produces(MediaType.APPLICATION_JSON)
-		@Consumes(MediaType.APPLICATION_JSON)
-		public Policy createPolicy(Policy policy){
-			System.out.println(policy.getPolicyNumber()+","+policy.getState());
-			policy.setInsuredLName("new Name");
-			return policy;
+		@Path("add")
+		//@Produces(MediaType.APPLICATION_JSON)
+		public String createPolicy(MultivaluedMap<String,String> formData,
+				@HeaderParam("user-agent") String ua){
+			System.out.println("Data >>"+formData+", ua value:"+ua);
+			return "Added";
+		}
+		
+		@GET
+		@Path("/getPolicies")
+		@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+		public Policy[] getPolicies(){
+			Policy[] policyArr = new Policy[2];
+			policyArr[0]=new Policy("AZAP00001","AZ","Hello F","Hello L");
+			policyArr[1]=new Policy("NVAP00001","NV","Hello F1","Hello L2");
+			return policyArr;
 		}
 }
